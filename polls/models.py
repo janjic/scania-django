9e738986f6e4e7c9aa233b96c3d3c43c89cddeb0
Branko Janjic <branko.janjic@fsd.rs>
2018-02-09 11:40:31 +0100

    Commit

--- polls/models.py
+++ polls/models.py
@@ -289,7 +289,7 @@ class Calculation(models.Model):
             ('dejan_koleska', 'Dejan Koleška')), blank=True, null=True)
 
     def __str__(self):
-        return self.id
+        return str(self.id)
 
 
 @python_2_unicode_compatible
